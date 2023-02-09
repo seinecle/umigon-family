940269416e186bd1ec3f8eb15f94101ac2c588f6
Clement Levallois <1244100+seinecle@users.noreply.github.com>
2023-02-09 17:12:03 +0100

    added rules to manage -ed and -ing terminaisons in English

--- umigon-lemmatizer-lightweight/src/main/java/net/clementlevallois/lemmatizerlightweight/Lemmatizer.java
+++ umigon-lemmatizer-lightweight/src/main/java/net/clementlevallois/lemmatizerlightweight/Lemmatizer.java
@@ -15,7 +15,7 @@ import java.util.Set;
  */
 public class Lemmatizer {
 
-    private String[] noLemmaEN = new String[]{"access", "accumbens", "addresses", "afterwards", "always", "amazing", "approaches", "analyses", "biases", "businesses", "ceiling", "classes", "crises", "daunting", "discusses", "economics", "elsevier", "ethics", "focuses", "fries", "goes", "humanities", "hypotheses", "inches", "lies", "losses", "marketing", "morning", "news", "outlier", "outstanding", "physics", "politics", "premises", "processes", "red", "rigged", "ries", "series", "sometimes", "something", "species", "spring", "status", "ted", "themselves", "neural processes", "united", "wales", "witnesses"};
+    private String[] noLemmaEN = new String[]{"access", "accumbens", "addresses", "afterwards", "always", "amazing", "approaches", "analyses", "biases", "businesses", "ceiling", "classes", "crises", "daunting", "discusses", "during", "economics", "elsevier", "ethics", "focuses", "fries", "goes", "humanities", "hypotheses", "inches", "lies", "losses", "marketing", "morning", "news", "outlier", "outstanding", "physics", "politics", "premises", "processes", "red", "rigged", "ries", "series", "sometimes", "something", "species", "spring", "status", "ted", "themselves", "neural processes", "united", "wales", "witnesses"};
     private String[] noLemmaFR = new String[]{"accès", "alors", "apres", "après", "aupres", "auprès", "Calvados", "concours", "corps", "cours", "dans", "discours", "divers", "etes", "êtes", "ethos", "éthos", "gens", "gros", "lors", "outils", "pays", "parcours", "pres", "près", "proces", "procès", "propos", "puis", "sans", "secours", "sens", "sommes", "succès", "succes", "temps", "toujours", "travers", "très", "tres", "univers", "viens", "vos"};
     private String[] noLemma = new String[]{"analytics", "accumbens", "aws", "bayes", "business", "charles", "ects", "cnrs", "cowles", "deep learning", "developer", "ethos", "faas", "forbes", "iaas", "james", "keynes", "koopmans", "nhs", "paas", "paris", "programming", "reactjs", "saas", "siemens", "sanders", "ted", "vuejs", "united states"};
 
@@ -73,12 +73,15 @@ public class Lemmatizer {
                 term = term.substring(0, term.length() - 2);
             }
             if (term.endsWith("ies")) {
-                term = term.substring(0, term.length() - 3) + "y";
-            }
-            else if (term.endsWith("'s")) {
+                if (!term.equals("movies")) {
+                    term = term.substring(0, term.length() - 3) + "y";
+                } else {
+                    term = term.substring(0, term.length() - 1);
+                }
+            } else if (term.endsWith("'s")) {
                 term = term.substring(0, term.length() - 2);
             } else if (term.endsWith("ed")) {
-                if (term.endsWith("rred")) {
+                if (term.endsWith("rred") || term.endsWith("mmed")) {
                     term = term.substring(0, term.length() - 3);
                 } else if (term.endsWith("lked")
                         || term.endsWith("cked")
@@ -86,7 +89,8 @@ public class Lemmatizer {
                         || term.endsWith("ssed")
                         || term.endsWith("lled")
                         || term.endsWith("iased")
-                        || (term.endsWith("red") && (!term.endsWith("ired")& !term.endsWith("ured")))
+                        || (term.endsWith("red") && (!term.endsWith("ired") & !term.endsWith("ured") & !term.endsWith("ared")))
+                        || (term.endsWith("med") && (!term.endsWith("framed") & !term.endsWith("lamed") & !term.endsWith("named") & !term.endsWith("shamed")))
                         || term.endsWith("aired")
                         || term.endsWith("used")
                         || term.endsWith("ned")
@@ -111,8 +115,10 @@ public class Lemmatizer {
                 } else if (term.endsWith("sing")
                         || term.endsWith("zing")
                         || term.endsWith("cing")
+                        || (term.endsWith("ting") && !term.endsWith("sting") && !term.endsWith("tting"))
+                        || (term.endsWith("ming") && (term.endsWith("framing") || !term.endsWith("laming") || !term.endsWith("naming") || !term.endsWith("shaming")))
                         || term.endsWith("ving")
-                        || term.endsWith("ring")) {
+                        || term.endsWith("ring") & !term.endsWith("during")) {
                     term = term.substring(0, term.length() - 3) + "e";
                 } else if (term.length() > 2) {
                     term = term.substring(0, term.length() - 3);
@@ -125,10 +131,12 @@ public class Lemmatizer {
                         }
                         size = term.length();
                         if (size > 1) {
-                            //voting has become vot. Should become vote. Same for any word ending in at or ot (not plant, suspect, ...). Yet, it will miscorrect "pivot"
+                            //voting has become vot. Should become vote. Same for any word ending in at or ot (not plant, suspect, ...). Yet, it will miscorrect "pivot" and "format"
                             lastTwoLetters = term.substring(size - 2, size);
-                            if (lastTwoLetters.equals("at") || lastTwoLetters.equals("ot") || lastTwoLetters.equals("id")) {
-                                term = term + "e";
+                            if (!term.equals("pivot") && !term.equals("format")) {
+                                if (lastTwoLetters.equals("at") || lastTwoLetters.equals("ot") || lastTwoLetters.equals("id")) {
+                                    term = term + "e";
+                                }
                             }
                         }
                     }
